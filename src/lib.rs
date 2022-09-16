1abd5eeaf0f2f39ec4ec64ff7d485e03a9d2496a
GodOfProgramming <jonathan.dickert@protonmail.com>
2022-09-16 16:08:33 -0400

    added prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 pub use tfix_derive::fixture;
 
+pub mod prelude {
+  pub use super::TestFixture;
+  pub use tfix_derive::fixture;
+}
+
 pub trait TestFixture: Sized {
-    fn set_up() -> Self;
-    fn tear_down(self) {}
+  fn set_up() -> Self;
+  fn tear_down(self) {}
 }
 
 #[cfg(test)]
